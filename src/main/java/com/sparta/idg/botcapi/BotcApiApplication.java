33c85c722374347c813018d88cc97301f2d90d5b
Diana Gall <dianagall47@gmail.com>
2024-06-18 09:20:10 +0100

    first commit

---
+++ src/main/java/com/sparta/idg/botcapi/BotcApiApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.idg.botcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotcApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotcApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/idg/botcapi/BotcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.idg.botcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotcApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
