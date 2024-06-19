1e437cdab159acd6ff62b050872745cbf502f661
Diana Gall <dianagall47@gmail.com>
2024-06-19 16:13:11 +0100

    adds complete crud functionality to scriptController

--- src/main/java/com/sparta/idg/botcapi/controllers/rest/ScriptRestController.java
+++ src/main/java/com/sparta/idg/botcapi/controllers/rest/ScriptRestController.java
@@ -3,10 +3,7 @@ package com.sparta.idg.botcapi.controllers.rest;
 import com.sparta.idg.botcapi.model.entities.Script;
 import com.sparta.idg.botcapi.model.repositories.ScriptRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,7 +16,7 @@ public class ScriptRestController {
         this.scriptRepository = scriptRepository;
     }
 
-    @GetMapping("/rest/allscripts")
+    @GetMapping("/rest/scripts")
     public List<Script> getAllScripts() {
        return scriptRepository.findAll();
     }
@@ -29,6 +26,35 @@ public class ScriptRestController {
         return scriptRepository.findScriptById(id);
     }
 
-    
+    @GetMapping("/rest/script/title")
+    public List<Script> getScriptByTitle(@RequestParam(name = "title", required = false) String title){
+       return scriptRepository.findAll().stream().filter(script -> script.getTitle().contains(title)).toList();
+    }
+
+    @PostMapping("/rest/script/new")
+    public String addScript(@RequestBody Script script){
+       scriptRepository.save(script);
+       return  "New script added: " + script.getTitle();
+    }
+
+    @DeleteMapping("/rest/script/delete/id{id}")
+    public String deleteScriptById(@PathVariable Integer id){
+       if(scriptRepository.existsById(id)){
+           scriptRepository.deleteById(id);
+           return "You have now deleted the script with id number: " + id;
+       }else{
+           return "Script with id "+ id + " wasn't found and therefore could not be deleted.";
+       }
+    }
+
+    @PutMapping("/rest/script/update/id{id}")
+    public Script updateScriptById(@PathVariable Integer id, @RequestBody Script script){
+       //Get the script that needs to be updated
+        Script scriptToUpdate = scriptRepository.findById(id).get();
+        //Update that script object with the changes
+        scriptToUpdate.setTitle(script.getTitle());
+        //Save it to the database
+        return scriptRepository.save(scriptToUpdate);
+    }
 
 }
